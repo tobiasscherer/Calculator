dff1ffce3f46e206dded4111c04a66a5dfdda1b5
tobia <tobia@10.207.9.165>
2017-03-24 19:40:03 +0100

    add comments

--- Fakultaet/src/ch/fhnw/mas/PHTO/MathUtil.java
+++ Fakultaet/src/ch/fhnw/mas/PHTO/MathUtil.java
@@ -3,13 +3,13 @@ package ch.fhnw.mas.PHTO;
 public class MathUtil {
 	/**
 	 * here comes some description.
-	 * @param grenze: limit number
+	 * @param limit: limit number
 	 * @return: go back
 	 */
 	static long faculty(int limit){
 		long fakultaet = 1;
 /**
- * zahl=1 is loop number
+ * iterator=1 is loop number
  */
 		for(int iterator=1; iterator<=limit; iterator++){
 			fakultaet = fakultaet * iterator;
